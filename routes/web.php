81306dbccab3aa6e40a2040b6b72441417bb12e1
DheCodesLy <dhealybia@gmail.com>
2026-04-10 11:23:48 +0700

    route login

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::resource('jenis-kendaraan', JenisKendaraanController::class);
 Route::resource('jenis-pemilik', JenisPemilikController::class);
